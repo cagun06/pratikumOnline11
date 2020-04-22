0d4a3877d641ec3a9fa0f5af5b1aed328c4575b2
candra.gunawan22.co.id@gmail.com <candra.gunawan22.co.id@gmail.com>
2020-04-22 21:02:51 +0700

    CorountineandRoom

---
+++ app/src/main/java/com/example/coroutineandroom/catatan/NoteDatabase.kt
@@ -0,0 +1,54 @@
+package com.example.coroutineandroom.catatan
+
+import android.content.Context
+import android.os.AsyncTask
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+
+@Database(entities = [Note::class],version = 1)
+abstract class NoteDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        private var instance: NoteDatabase? = null
+
+        @InternalCoroutinesApi
+        fun getInstance(context: Context): NoteDatabase? {
+            if (instance == null) {
+                synchronized(NoteDatabase::class) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        NoteDatabase::class.java, "note_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .addCallback(roomCallback)
+                        .build()
+                }
+            }
+            return instance
+        }
+
+        fun destroyInstance() {
+            instance = null
+        }
+
+        private val roomCallback = object : RoomDatabase.Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                PopulationDbAsyncTask(instance)
+                    .execute()
+            }
+        }
+    }
+
+    class PopulationDbAsyncTask(db: NoteDatabase?) : AsyncTask<Unit, Unit, Unit>() {
+        private val noteDao = db?.noteDao()
+        override fun doInBackground(vararg p0: Unit?) {
+            noteDao?.insert(Note("Coba 1", "Deskripsi 1", 1))
+        }
+    }
+}
